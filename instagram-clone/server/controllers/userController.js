5e3779c588f7110e6232a9d0d3b80f2d8c988828
nigrojin <nigrojin@naver.com>
2023-05-31 21:44:40 +0900

    .

---
+++ instagram-clone/server/controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require("../models/User"); // User 모델
+const fileHandler = require('../utils/fileHandler'); // 파일 처리 패키지
+// 유효성 검사 패키지
+const { check, validationResult } = require('express-validator');
+
+
+// 회원가입 로직
+exports.create = [];
+
+// 정보 수정 로직
+exports.update = []
+
+// 로그인 로직
+exports.login = [];
